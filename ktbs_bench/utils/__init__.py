871728c352f0275604470e01c44aac2db21ac8bb
vincent <vincent-octo@users.noreply.github.com>
2014-02-17 12:52:56 +0000

    add utils folder

---
+++ ktbs_bench/utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['timer']
+
+from . import timer
---
+++ ktbs_bench/utils/timer.py
@@ -0,0 +1,27 @@
+import resource
+from time import clock
+
+
+class Timer:
+    """Measure process duration."""
+    def __init__(self, tick_now=True):
+        self.start_time = []
+        if tick_now:
+            self.start_time = self.tick()
+        self.stop_time = None
+        self.delta = None
+
+    def tick(self):
+        """Return usr, sys and real times."""
+        times = list(resource.getrusage(resource.RUSAGE_SELF)[:2])
+        times.append(clock())
+        return times
+
+    def stop(self):
+        """Stop the timer and compute delta time."""
+        self.stop_time = self.tick()
+        self.delta = [stop - start for start, stop in zip(self.start_time, self.stop_time)]
+
+    def __repr__(self):
+        return 'usr: %s\tsys: %s\tusr+sys: %s\t real: %s' %\
+               (self.delta[0], self.delta[1], self.delta[0]+self.delta[1], self.delta[2])
