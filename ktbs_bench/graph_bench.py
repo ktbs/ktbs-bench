965bd9e2b2452e2a3c8b1e0096d26a152f9a7ffa
vincent <vincent-octo@users.noreply.github.com>
2014-02-18 11:33:47 +0000

    First try at implementing graph handling in graph_bench

--- ktbs_bench/graph_bench.py
+++ ktbs_bench/graph_bench.py
@@ -7,36 +7,34 @@ from utils.timer import Timer
 
 
 class GraphBench:
-    def __init__(self, output_file, time_indicator):
+    def __init__(self, graph_dict, output_file, time_indicator):
         self.results = {}  # Results row: function, column: graph
-        self.list_graph = ['NoneDB']
+        self.graph_dict = graph_dict
         self.output_file = output_file
         self.time_indicator = time_indicator
         # Generate csv header
         with open(self.output_file, 'wb') as csv_file:
-            csv_writer = csv.DictWriter(csv_file, ['func_name'] + self.list_graph)
+            csv_writer = csv.DictWriter(csv_file, ['func_name'] + self.graph_dict.keys())
             csv_writer.writeheader()
 
     def bench(self, func):
         """Decorator to run a benchmark on selected function"""
-        timer = Timer(tick_now=False)  # don't tick now, func() is only called on run()
-
         # Make a timer wrapper, func() is executed only when time_wrapper() is called
         @wraps(func)
-        def time_wrapper(*args, **kwargs):
+        def time_wrapper(timer, graph):
             timer.start()
-            func(*args, **kwargs)
+            func(graph)
             timer.stop()
             return timer.get_times()
 
         # Benchmark func against each graph
         row = {'func_name': func.__name__}
-        for graph_name in self.list_graph:
-            times = time_wrapper()
+        for graph_name in self.graph_dict.keys():
+            times = time_wrapper(Timer(tick_now=False), self.graph_dict[graph_name])
             self.results[func.__name__] = {graph_name: times}
             row[graph_name] = times[self.time_indicator]  # get real time for func
 
         # Write bench to csv
         with open(self.output_file, 'ab') as csv_file:
-            csv_writer = csv.DictWriter(csv_file, ['func_name'] + self.list_graph)
+            csv_writer = csv.DictWriter(csv_file, ['func_name'] + self.graph_dict.keys())
             csv_writer.writerow(row)
