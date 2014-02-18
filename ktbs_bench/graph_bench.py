6c0acf7a0cedfc341757b2301a5347a2c0e5ebe7
vincent <vincent-octo@users.noreply.github.com>
2014-02-18 10:35:22 +0000

    Fix graph_bench to use timer dict

--- ktbs_bench/graph_bench.py
+++ ktbs_bench/graph_bench.py
@@ -7,10 +7,11 @@ from utils.timer import Timer
 
 
 class GraphBench:
-    def __init__(self, output_file=None):
+    def __init__(self, output_file, time_indicator):
         self.results = {}  # Results row: function, column: graph
         self.list_graph = ['NoneDB']
         self.output_file = output_file
+        self.time_indicator = time_indicator
         # Generate csv header
         with open(self.output_file, 'wb') as csv_file:
             csv_writer = csv.DictWriter(csv_file, ['func_name'] + self.list_graph)
@@ -20,6 +21,7 @@ class GraphBench:
         """Decorator to run a benchmark on selected function"""
         timer = Timer(tick_now=False)  # don't tick now, func() is only called on run()
 
+        # Make a timer wrapper, func() is executed only when time_wrapper() is called
         @wraps(func)
         def time_wrapper(*args, **kwargs):
             timer.start()
@@ -32,7 +34,7 @@ class GraphBench:
         for graph_name in self.list_graph:
             times = time_wrapper()
             self.results[func.__name__] = {graph_name: times}
-            row[graph_name] = times[2]  # get real time for func
+            row[graph_name] = times[self.time_indicator]  # get real time for func
 
         # Write bench to csv
         with open(self.output_file, 'ab') as csv_file:
