03f0932b8a54fd6d53a9dfde54fd94042def4963
vincent <vincent-octo@users.noreply.github.com>
2014-02-19 09:54:20 +0000

    Add example of bench query with selected queries from sp2bench

---
+++ bench_examples/bench_query.py
@@ -0,0 +1,166 @@
+from ktbs_bench import graph_store as gs
+from ktbs_bench import graph_bench
+from os import path
+import rdflib as r
+
+
+# Setting up the graph to have some things to read
+postgres = r.Graph(store='SQLAlchemy')
+postgres.open("postgresql+psycopg2://localhost/newtest_sqapg", True)
+
+data_dir = '../data'
+data_file = path.join(path.abspath(path.dirname(__file__)), data_dir, '8000.n3')
+
+postgres.parse(source=data_file, format="n3")
+postgres.close()
+
+# Make a graph store
+gb_postgres = gs.GraphStore(store='SQLAlchemy',
+                            connect_args=["postgresql+psycopg2://localhost/newtest_sqapg", True])
+
+gb_sleepycat = gs.GraphStore(connect_args=['/tmp/sc.db', True])
+
+graph_dict = {'postgres': gb_postgres, 'sleepycat': gb_sleepycat}
+
+# Start benchmarking
+gb = graph_bench.GraphBench(graph_dict, "/tmp/test.csv", time_indicator='real', n_repeat=1)
+
+
+@gb.bench
+def query_all(graph):
+    graph.query('select * where {?s ?p ?o}')
+
+
+@gb.bench
+def query_sp2b_q1(graph):
+    graph.query("""
+        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX dc:      <http://purl.org/dc/elements/1.1/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX bench:   <http://localhost/vocabulary/bench/>
+        PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
+
+        SELECT ?yr
+        WHERE {
+          ?journal rdf:type bench:Journal .
+          ?journal dc:title "Journal 1 (1940)"^^xsd:string .
+          ?journal dcterms:issued ?yr
+        }""")
+
+
+@gb.bench
+def query_sp2b_q2(graph):
+    graph.query("""
+        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX swrc:    <http://swrc.ontoware.org/ontology#>
+        PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+        PREFIX bench:   <http://localhost/vocabulary/bench/>
+        PREFIX dc:      <http://purl.org/dc/elements/1.1/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+
+        SELECT ?inproc ?author ?booktitle ?title
+               ?proc ?ee ?page ?url ?yr ?abstract
+        WHERE {
+          ?inproc rdf:type bench:Inproceedings .
+          ?inproc dc:creator ?author .
+          ?inproc bench:booktitle ?booktitle .
+          ?inproc dc:title ?title .
+          ?inproc dcterms:partOf ?proc .
+          ?inproc rdfs:seeAlso ?ee .
+          ?inproc swrc:pages ?page .
+          ?inproc foaf:homepage ?url .
+          ?inproc dcterms:issued ?yr
+          OPTIONAL {
+            ?inproc bench:abstract ?abstract
+          }
+        }
+        ORDER BY ?yr""")
+
+
+@gb.bench
+def query_sp2b_q3a(graph):
+    graph.query("""
+        PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX bench: <http://localhost/vocabulary/bench/>
+        PREFIX swrc:  <http://swrc.ontoware.org/ontology#>
+
+        SELECT ?article
+        WHERE {
+          ?article rdf:type bench:Article .
+          ?article ?property ?value
+          FILTER (?property=swrc:pages)
+        }""")
+
+
+@gb.bench
+def query_sp2b_q3b(graph):
+    graph.query("""
+        PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX bench: <http://localhost/vocabulary/bench/>
+        PREFIX swrc:  <http://swrc.ontoware.org/ontology#>
+
+        SELECT ?article
+        WHERE {
+          ?article rdf:type bench:Article .
+          ?article ?property ?value
+          FILTER (?property=swrc:month)
+        }""")
+
+
+@gb.bench
+def query_sp2b_q3c(graph):
+    graph.query("""
+        PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX swrc:  <http://swrc.ontoware.org/ontology#>
+        PREFIX bench: <http://localhost/vocabulary/bench/>
+
+        SELECT ?article
+        WHERE {
+          ?article rdf:type bench:Article .
+          ?article ?property ?value
+          FILTER (?property=swrc:isbn)
+        }""")
+
+
+@gb.bench
+def query_sp2b_q4(graph):
+    graph.query("""
+        PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX bench:   <http://localhost/vocabulary/bench/>
+        PREFIX dc:      <http://purl.org/dc/elements/1.1/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+        PREFIX swrc:    <http://swrc.ontoware.org/ontology#>
+
+        SELECT DISTINCT ?name1 ?name2
+        WHERE {
+          ?article1 rdf:type bench:Article .
+          ?article2 rdf:type bench:Article .
+          ?article1 dc:creator ?author1 .
+          ?author1 foaf:name ?name1 .
+          ?article2 dc:creator ?author2 .
+          ?author2 foaf:name ?name2 .
+          ?article1 swrc:journal ?journal .
+          ?article2 swrc:journal ?journal
+          FILTER (?name1<?name2)
+        }""")
+
+
+@gb.bench
+def query_sp2b_q9(graph):
+    graph.query("""
+        PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+        SELECT DISTINCT ?predicate
+        WHERE {
+          {
+            ?person rdf:type foaf:Person .
+            ?subject ?predicate ?person
+          } UNION {
+            ?person rdf:type foaf:Person .
+            ?person ?predicate ?object
+          }
+        }
+    """)
