799b935b5b01cf538a6a07309c845fda60c04700
vincent <vincent-octo@users.noreply.github.com>
2014-02-20 09:20:20 +0000

    Make call_signature a function

--- ktbs_bench/utils/decorators.py
+++ ktbs_bench/utils/decorators.py
@@ -11,17 +11,21 @@ def bench(f):
 
     @wraps(f)
     def wrapped(*args, **kwargs):
-        call_signature = getcallargs(f, *args, **kwargs)
-
         timer.start()
         f(*args, **kwargs)
         timer.stop()
 
-        res = [';'.join(["%s=%s" % (k, v) for k, v in call_signature.items()]),
+        res = [call_signature(f, *args, **kwargs),
                timer.get_times()['real']]  # TODO penser a quel temps garder
         return res
 
     return wrapped
+
+
+def call_signature(f, *args, **kwargs):
+    """Return a string representation of a function call"""
+    call_args = getcallargs(f, *args, **kwargs)
+    return ';'.join(["%s=%s" % (k, v) for k, v in call_args.items()])
 
 
 @bench
