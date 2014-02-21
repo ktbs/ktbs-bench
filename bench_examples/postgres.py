5c83b0b7a4146eced48e7640c657928a11e1ab21
vincent <vincent-octo@users.noreply.github.com>
2014-02-21 09:43:45 +0000

    Add example of postgres GraphStore

---
+++ bench_examples/postgres.py
@@ -0,0 +1,21 @@
+from ktbs_bench import graph_store as gs
+import psycopg2 as pg
+
+
+def pg_create(db_name, db_user):
+    """Create a PostgreSQL database."""
+    connection = pg.connect(
+        "dbname=%s user=%s" % (db_user, db_user))  # connect to db_user as db_name might not been created yet
+    connection.set_isolation_level(pg.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE %s" % db_name)
+    cursor.close()
+    connection.close()
+
+
+def get_store(db_name, db_user):
+    postgres_store = gs.GraphStore(store="SQLAlchemy",
+                                   connect_args={'configuration': "postgresql+psycopg2://localhost/%s" % db_name,
+                                                 'create': True},
+                                   create_func=pg_create, create_args=[db_name, db_user])
+    return postgres_store
