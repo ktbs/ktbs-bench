37b5b5185182cafdaa0a89acb46c064976e546e7
vincent <vincent-octo@users.noreply.github.com>
2014-02-21 08:57:56 +0000

    Improve connect() of GraphStore

--- ktbs_bench/graph_store.py
+++ ktbs_bench/graph_store.py
@@ -7,4 +7,12 @@ class GraphStore(Graph):
         self.connect_args = connect_args
 
     def connect(self):
-        self.open(*self.connect_args)
+        if isinstance(self.connect_args, list):
+            self.open(*self.connect_args)
+        elif isinstance(self.connect_args, dict):
+            self.open(**self.connect_args)
+        else:
+            raise TypeError("connect_args must be either a list or a dict.")
+
+    def create(self):
+        pass
