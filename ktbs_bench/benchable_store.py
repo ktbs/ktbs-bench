5794d0ff86f90a1f1f2ad7ca52cb6f1d34cb5b24
vincent <vincent-octo@users.noreply.github.com>
2014-02-25 08:27:56 +0000

    Make Graph an attribute rather than an inheritance

--- ktbs_bench/benchable_store.py
+++ ktbs_bench/benchable_store.py
@@ -2,33 +2,36 @@ from rdflib import Graph
 from ktbs_bench.bnsparqlstore import SPARQLStore
 
 
-class BenchableStore(Graph):
-    def __init__(self, connect_args, create_func=None, create_args=[], *args, **kwargs):
-        super(BenchableStore, self).__init__(*args, **kwargs)
-        self.connect_args = connect_args
-        self.create_func = create_func
-        self.create_args = create_args
+class BenchableStore:
+    """Allows to use a store/graph for benchmarks.
 
-    def connect(self):
-        if isinstance(self.connect_args, dict):
-            self.open(**self.connect_args)
+    Contains a rdflib.Graph with setup and teardown.
+    """
+
+    def __init__(self, store, graph_id, store_config, store_create=False):
+        self.graph = Graph(store=store, identifier=graph_id)
+        self._store_config = store_config
+        self._store_create = store_create
+
+    def connect(self, store_create=None):
+        if store_create:
+            do_create = store_create
         else:
-            raise TypeError("connect_args must be a dict.")
+            do_create = self._store_create
+        self.graph.open(self._store_config, create=do_create)
 
-    def create(self):
-        if self.create_func:
-            self.create_func(*self.create_args)  # TODO gerer exception si db existe deja
+    def close(self, commit_pending_transaction=False):
+        self.graph.close(commit_pending_transaction=commit_pending_transaction)
 
     def destroy(self):
-        """For SQL: destroy tables of the DB, not the DB itself."""
-        if isinstance(self.store, SPARQLStore):
+        if isinstance(self.graph.store, SPARQLStore):
             self.sparql_destroy()
         else:
-            super(BenchableStore, self).destroy(self.connect_args['configuration'])
+            self.graph.destroy(self._store_config)
 
     def sparql_destroy(self):
         """Try to destroy the graph as if the current store is a SPARQLStore."""
         # TODO improve destroy by using SPARQL CLEAR GRAPH if RDFLib supports it
         # or execute something on the command line
-        for s, p, o in self:
-            self.remove((s, p, o))
+        for s, p, o in self.graph:
+            self.graph.remove((s, p, o))
