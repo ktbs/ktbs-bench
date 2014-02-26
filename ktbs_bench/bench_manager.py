04c6bcc62063d639ce30cad0fb6a09b6a17b904d
vincent <vincent-octo@users.noreply.github.com>
2014-02-26 10:32:33 +0000

    Add writing CSV output to run()

--- ktbs_bench/bench_manager.py
+++ ktbs_bench/bench_manager.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from csv import DictWriter
 
 from ktbs_bench.utils.decorators import bench as util_bench
 
@@ -19,8 +20,9 @@ class BenchManager:
         func = contextmanager(func)
         self._contexts.append(func)
 
-    def run(self, output_file):
+    def run(self, output_filename):
         """Execute each collected function against each context."""
+        # Run the bench for each function against each context
         for func in self._bench_funcs:
             self._results[func.__name__] = {}
             for context in self._contexts:
@@ -29,3 +31,15 @@ class BenchManager:
                         arg = tuple([arg])  # Make arg a tuple of one element
                     _, res_time = func(*arg)
                 self._results[func.__name__][context.__name__] = res_time
+
+        # Write output to a CSV file
+        fieldnames = ['func_name']
+        fieldnames += [context.__name__ for context in self._contexts]
+        with open(output_filename, 'wb') as output_file:
+            csv_file = DictWriter(output_file, fieldnames)
+            csv_file.writeheader()
+
+            for func_name, env_results in self._results.items():
+                row_results = env_results
+                row_results['func_name'] = func_name
+                csv_file.writerow(row_results)
