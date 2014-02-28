c0fc6c400ca9fa1415702a1ba638bb28820d824d
vincent <vincent-octo@users.noreply.github.com>
2014-02-28 17:02:09 +0000

    Fix test to use py.test instead of legacy attest
    Note: all tests are reporting ok

--- tests/test_benchable_store_postgres.py
+++ tests/test_benchable_store_postgres.py
@@ -1,23 +1,16 @@
-from attest import Tests
 from ktbs_bench.benchable_store import BenchableStore
 import rdflib
 
-BS_POSTGRES = Tests()
 
 # Previously created store are here
 CREATED_STORE = {'store_id': 'http://localhost/bs/pg/persistent_store',
                  'config': 'postgresql+psycopg2://localhost/persistent_store'}
 
 
-@BS_POSTGRES.test
-def connect_existing_pg():
+def test_connect_existing_pg():
     postgres = BenchableStore("SQLAlchemy",
                               CREATED_STORE['store_id'],
                               CREATED_STORE['config'],
                               store_create=False)
     assert postgres.connect() == rdflib.store.VALID_STORE
     postgres.close()
-
-
-if __name__ == '__main__':
-    BS_POSTGRES.run()
--- tests/test_benchable_store_virtuoso.py
+++ tests/test_benchable_store_virtuoso.py
@@ -1,100 +1,93 @@
 from uuid import uuid1
 
+import pytest
 from ktbs_bench.benchable_store import BenchableStore
 import rdflib
 
 
-VIRTUOSO_CONNECT = Tests()
+# This store should be running when running the tests
 EMPTY_STORE = {'store_id': 'http://localhost/bs/virtuoso/test/empty_store/',
                'config': ("http://localhost:8890/sparql/", "http://localhost:8890/sparql/")}
 
-VIRTUOSO_NOT_EMPTY = Tests()
 
+class TestVirtuoso:
+    def test_fail_connect_query(self):
+        """Test that the store should not connect if the query endpoint is wrong."""
+        bad_query_endpoint = 'http://should_fail/'
+        virtuoso = BenchableStore("SPARQLUpdateStore",
+                                  EMPTY_STORE['store_id'],
+                                  (bad_query_endpoint, EMPTY_STORE['config'][1]),
+                                  store_create=False)
+        fail = False
+        try:
+            virtuoso.connect()
+            # Doing a query forces RDFLib to actually connect to the query endpoint
+            virtuoso.graph.query('select * where {?s ?p ?o}')
+        except:
+            fail = True
+        assert fail
 
-@VIRTUOSO_CONNECT.test
-def fail_connect_query():
-    """Test that the store should not connect if the query endpoint is wrong."""
-    bad_query_endpoint = 'http://should_fail/'
-    virtuoso = BenchableStore("SPARQLUpdateStore",
-                              EMPTY_STORE['store_id'],
-                              (bad_query_endpoint, EMPTY_STORE['config'][1]),
-                              store_create=False)
-    fail = False
-    try:
-        virtuoso.connect()
-        # Doing a query forces RDFLib to actually connect to the query endpoint
-        virtuoso.graph.query('select * where {?s ?p ?o}')
-    except:
-        fail = True
-    assert fail
-
-
-@VIRTUOSO_CONNECT.test
-def fail_connect_update():
-    """Test that the store should not connect if the update endpoint is wrong."""
-    bad_update_endpoint = 'http://should_fail'
-    virtuoso = BenchableStore('SPARQLUpdateStore',
-                              EMPTY_STORE['store_id'],
-                              (EMPTY_STORE['config'][0], bad_update_endpoint),
-                              store_create=False)
-    fail = False
-    triple = (rdflib.URIRef('s'), rdflib.URIRef('p'), rdflib.URIRef('o'))
-    try:
-        virtuoso.connect()
-        # Doing an add forces RDFLib to actually connect to the update endpoint
-        virtuoso.graph.add(triple)
-    except:
-        fail = True
-    else:
-        virtuoso.graph.remove(triple)
-    assert fail
-
-
-@VIRTUOSO_CONNECT.test
-def succeed_connect():
-    """Test if the server is up."""
-    virtuoso = BenchableStore('SPARQLUpdateStore',
-                              EMPTY_STORE['store_id'],
-                              EMPTY_STORE['config'],
-                              store_create=False)
-    succeed = True
-    try:
-        virtuoso.connect()
-        virtuoso.graph.query('select * where {?s ?p ?o}')
-    except:
-        succeed = False
-    assert succeed
-
-
-@VIRTUOSO_NOT_EMPTY.context
-def connect():
-    try:
+    def test_fail_connect_update(self):
+        """Test that the store should not connect if the update endpoint is wrong."""
+        bad_update_endpoint = 'http://should_fail'
         virtuoso = BenchableStore('SPARQLUpdateStore',
-                                  'http://localhost/bs/virtuoso/test/%s' % uuid1(),
+                                  EMPTY_STORE['store_id'],
+                                  (EMPTY_STORE['config'][0], bad_update_endpoint),
+                                  store_create=False)
+        fail = False
+        triple = (rdflib.URIRef('s'), rdflib.URIRef('p'), rdflib.URIRef('o'))
+        try:
+            virtuoso.connect()
+            # Doing an add forces RDFLib to actually connect to the update endpoint
+            virtuoso.graph.add(triple)
+        except:
+            fail = True
+        else:
+            virtuoso.graph.remove(triple)
+        assert fail
+
+    def test_succeed_connect(self):
+        """Test if the server is up."""
+        virtuoso = BenchableStore('SPARQLUpdateStore',
+                                  EMPTY_STORE['store_id'],
+                                  EMPTY_STORE['config'],
+                                  store_create=False)
+        succeed = True
+        try:
+            virtuoso.connect()
+            virtuoso.graph.query('select * where {?s ?p ?o}')
+        except:
+            succeed = False
+        assert succeed
+
+    @pytest.fixture(scope='module')
+    def non_empty_benchable_store(self, request):
+        graph_id = 'http://localhost/bs/virtuoso/test/%s' % uuid1()
+        virtuoso = BenchableStore('SPARQLUpdateStore',
+                                  graph_id,
                                   ("http://localhost:8890/sparql/", "http://localhost:8890/sparql/"),
                                   store_create=False)
         virtuoso.connect()
 
         # Put some triples in the graph
-        for i in xrange(100):
+        for i in xrange(10):
             virtuoso.graph.add((rdflib.URIRef('http://localhost/triple/s/%s' % i),
                                 rdflib.URIRef('http://localhost/triple/p/%s' % i),
                                 rdflib.URIRef('http://localhost/triple/o/%s' % i)))
-        yield virtuoso
-    finally:
-        pass  # Could have use close(), but not implemented for SPARQL store in RDFLib
 
+        # Print action to perform on teardown
+        def teardown():
+            # TODO should delete the graph from the store
+            pass
 
-@VIRTUOSO_NOT_EMPTY.test
-def test_destroy(benchable_store):
-    """Empty a graph, there should be not a single triple in the store after destroy()"""
-    # Remove all triples from the store
-    benchable_store.destroy()
+        request.addfinalizer(teardown)
+        return virtuoso
 
-    triple_count = len(benchable_store.graph)
-    assert triple_count == 0
+    # Used with non empty graph
+    def test_destroy(self, non_empty_benchable_store):
+        """Empty a graph, there should be not a single triple in the store after destroy()"""
+        # Remove all triples from the store
+        non_empty_benchable_store.destroy()
 
-
-if __name__ == '__main__':
-    VIRTUOSO_CONNECT.run()
-    VIRTUOSO_NOT_EMPTY.run()
+        triple_count = len(non_empty_benchable_store.graph)
+        assert triple_count == 0
